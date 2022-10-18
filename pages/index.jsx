aab35f53480e84748bb95e3f6dd52d7af3897e5c
Bitzz121 <abitsolihin8@gmail.com>
2022-10-18 15:33:23 +0700

    fix slider

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ import '@splidejs/react-splide/css';
 const index = () => {
 
   return (
-      <div className="container w-full min-h-[100vh] bg-[#121212]">
+      <div className="container max-w-[100vw] min-h-[100vh] bg-[#121212]">
       <nav className='w-full h-[60px] flex bg-[#121212]/75 shadow-md z-50'>
         <div className="logo p-4 w-[40%]">
           <h1 className='text-red-500 text-xl font-bold'>FILMAXX</h1>
@@ -35,11 +35,55 @@ const index = () => {
             <Splide className="absolute h-full w-full top-0 z-20 flex items-center text-white" options={{
               type: 'loop',
               autoplay: 'true',
+              perPage: 1,
+              perMove: 1,
               padding: '2rem',
               gap: '5px',
               focus: 'center',
               pagination: false,
             }}>
+              <SplideSlide className="card duration-200 w-full h-[50%] bg-white/[0.15] rounded-xl backdrop-blur-md flex items-center">
+                <div className="article w-[60%] h-full p-2">
+                  <div className="title h-[20%] w-full flex items-center text-2xl p-2">
+                    <h1>Harry Potter</h1>
+                  </div>
+                  <div className="description h-[50%] w-full text-xs flex items-center font-light p-2">
+                    <p className="text-ellipsis overflow-hidden">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, accusantium quam veniam quibusdam libero ullam aliquam explicabo consequuntur quos. Dolorum.</p>
+                  </div>
+                  <div className="rating-button h-[30%] w-full flex items-center p-2">
+                    <Link href="/"><a className="px-5 py-2 bg-red-600 text-xs rounded">Nonton</a></Link>
+                  </div>
+                </div>
+                <div className="img-wrapper w-[40%] h-full flex jutify-center items-center pr-4">
+                  <Image className="rounded-xl"
+                    src={harry}
+                    alt="Picture of the author"
+                    width="150px"
+                    height="200px"
+                  />
+                </div>
+              </SplideSlide>
+              <SplideSlide className="card duration-200 w-full h-[50%] bg-white/[0.15] rounded-xl backdrop-blur-md flex items-center">
+                <div className="article w-[60%] h-full p-2">
+                  <div className="title h-[20%] w-full flex items-center text-2xl p-2">
+                    <h1>Harry Potter</h1>
+                  </div>
+                  <div className="description h-[50%] w-full text-xs flex items-center font-light p-2">
+                    <p className="text-ellipsis overflow-hidden">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, accusantium quam veniam quibusdam libero ullam aliquam explicabo consequuntur quos. Dolorum.</p>
+                  </div>
+                  <div className="rating-button h-[30%] w-full flex items-center p-2">
+                    <Link href="/"><a className="px-5 py-2 bg-red-600 text-xs rounded">Nonton</a></Link>
+                  </div>
+                </div>
+                <div className="img-wrapper w-[40%] h-full flex jutify-center items-center pr-4">
+                  <Image className="rounded-xl"
+                    src={harry}
+                    alt="Picture of the author"
+                    width="150px"
+                    height="200px"
+                  />
+                </div>
+              </SplideSlide>
               <SplideSlide className="card duration-200 w-full h-[50%] bg-white/[0.15] rounded-xl backdrop-blur-md flex items-center">
                 <div className="article w-[60%] h-full p-2">
                   <div className="title h-[20%] w-full flex items-center text-2xl p-2">
