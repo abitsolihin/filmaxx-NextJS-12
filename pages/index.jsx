9c8ab562525af36c8e63a0156cd939a7efec5bf3
Bitzz121 <abitsolihin8@gmail.com>
2022-10-18 14:31:54 +0700

    fix navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../Components/Navbar"
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import Link from 'next/link'
 import harry from '../public/thumbnail.webp'
@@ -10,8 +9,27 @@ const index = () => {
 
   return (
     <div className="w-screen h-screen bg-[#121212]">
-      <Navbar />
-      <div className=" h-full w-full">
+      <nav className='w-full h-[60px] flex bg-[#121212]/75 fixed shadow-md z-50'>
+        <div className="logo p-4 w-[40%]">
+          <h1 className='text-red-500 text-xl font-bold'>FILMAXX</h1>
+        </div>
+        <div className="hamburger sm:hidden h-full w-full justify-center items-end gap-[5px] flex flex-col pr-8">
+          <span className='h-[3px] w-[30px] bg-white'></span>
+          <span className='h-[3px] w-[35px] bg-white'></span>
+          <span className='h-[3px] w-[40px] bg-white'></span>
+        </div>
+        <div className="right hidden sm:flex w-full">
+          <div className="navlist w-[80%] lg:w-[85%] text-white flex items-center justify-end text-sm">
+            <Link href="/"><a className="px-4 py-5 relative after:content-[''] after:bottom-0 after:h-[2px] hover:after:w-full after:w-0 after:absolute after:bg-red-600 flex flex-col justify-center items-center after:duration-200">Daftar Film</a></Link>
+            <Link href="/"><a className="px-4 py-5 relative after:content-[''] after:bottom-0 after:h-[2px] hover:after:w-full after:w-0 after:absolute after:bg-red-600 flex flex-col justify-center items-center after:duration-200">Kartun</a></Link>
+            <Link href="/"><a className="px-4 py-5 relative after:content-[''] after:bottom-0 after:h-[2px] hover:after:w-full after:w-0 after:absolute after:bg-red-600 flex flex-col justify-center items-center after:duration-200">Action</a></Link>
+          </div>
+          <div className="button w-[20%] lg:w-[15%] text-white flex items-center justify-center">
+            <Link href='/'><a className="hover:bg-red-600 border-2 border-solid border-red-600 duration-200 px-5 text-sm py-2 rounded" >Sign In</a></Link>
+          </div>
+        </div>
+      </nav>
+      <div className="section h-full w-full">
         <div className="background-container relative w-full h-[70%] after:content-[''] after:absolute after:z-10 after:top-0 after:left-0 after:w-[60%] after:h-full after:bg-gradient-to-r after:from-[#121212] after:to-transparent before:content-[''] before:absolute before:z-10 before:top-0 before:right-0 before:w-[60%] before:h-full before:bg-gradient-to-l before:from-[#121212] before:to-transparent">
           <div className="card-container absolute h-full w-full top-0 z-20 flex items-center justify-center text-white">
             <Splide className="absolute h-full w-full top-0 z-20 flex items-center text-white" options={{
