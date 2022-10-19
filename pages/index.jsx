416b8282bb4e8bbf2758b1134714de5dc633a218
Bitzz121 <abitsolihin8@gmail.com>
2022-10-19 18:24:08 +0700

    fix api

--- pages/index.jsx
+++ pages/index.jsx
@@ -75,13 +75,13 @@ export default function Index() {
               {films.map((item) => (
                 <SplideSlide className="card duration-200 w-full h-[70%] md:h-[300px] bg-white/25 rounded-xl backdrop-blur-xl flex items-center justify-center" key={item.id}>
                   <div className="article w-[60%] h-full p-2">
-                    <div className="title h-[30%] w-full flex items-center text-2xl md:text-3xl ">
+                    <div className="title md:h-[30%] h-[50px] truncate w-full flex items-center text-xl md:text-3xl ">
                       <h1 className='p-2'>{item.name}</h1>
                     </div>
-                    <div className="description h-[50%] w-full text-xs md:text-sm flex items-center md:items-start font-light p-2">
-                      <p className="text-ellipsis overflow-hidden">{item.sinopsis}</p>
+                    <div className="description h-[90px] md:h-[120px] overflow-ellipsis w-full text-xs md:text-sm flex items-center md:items-start font-light p-2">
+                      <p className="h-[80px] md:h-[120px] overflow-ellipsis overflow-y-hidden">{item.sinopsis}</p>
                     </div>
-                    <div className="rating-button h-[20%] w-full flex items-center p-2">
+                    <div className="rating-button h-[25%] w-full flex items-center p-2">
                       <div className="rating w-[50%] hidden md:flex items-center">
                         <div class="flex items-center justify-center text-white">
                           <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -95,7 +95,7 @@ export default function Index() {
                   </div>
                   <div className="img-wrapper w-[40%] h-full flex justify-center p-4">
                     <Image className="rounded-xl w-[300px] h-[400px]"
-                      src={`${item.url}`}
+                      src={item.url}
                       alt="Thumbnail"
                       width="300px"
                       height="400px"
@@ -136,17 +136,22 @@ export default function Index() {
                   },
                 },
               }} className="absolute top-0 h-full w-full flex justify-center">
-                <SplideSlide className="card h-full">
-                  <div className="img-wrapper overflow-hidden relative h-[150px] w-[100px] bg-harry bg-cover bg-center rounded-md group after:content-[''] after:absolute after:z-10 after:hover:bottom-0 after:-bottom-10 after:duration-200 after:left-0 after:w-full after:h-full after:bg-gradient-to-t after:from-[#121212] after:to-transparent after:rounded-md" >
+                {films.map((film) => {
+                  const {id,name,genre,url} = film
+                  return (
+                <SplideSlide className="card h-full" key={id}>
+                  <div style={{backgroundImage:`url(${url})`}} className="img-wrapper overflow-hidden relative h-[150px] w-[100px] bg-harry bg-cover bg-center rounded-md group after:content-[''] after:absolute after:z-10 after:hover:bottom-0 after:-bottom-20 after:duration-200 after:left-0 after:w-full after:h-full after:bg-gradient-to-t after:from-[#121212] after:to-transparent after:rounded-md" >
                     <div className="kategori absolute right-0 top-0 text-xs px-2 py-1 bg-red-600 rounded-tr-md rounded-bl-md">
-                      <h2>Movie</h2>
+                      <h2>{genre}</h2>
                     </div>
-                    <div className="title absolute group-hover:bottom-2 -bottom-10 duration-200 w-full flex justify-center
+                    <div className="title absolute group-hover:bottom-2 -bottom-20 duration-200 w-full flex justify-center
                   ">
-                      <h1 className="z-50 text-xs text-ellipsis">Harry Potter</h1>
+                      <h1 className="z-50 text-xs text-ellipsis">{name}</h1>
                     </div>
                   </div>
                 </SplideSlide>
+                  )
+                  })}
               </Splide>
             </div>
           </div>
