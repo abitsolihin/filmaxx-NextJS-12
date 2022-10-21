ecd1cf53343be7d226f438aec2426d89ecf716f7
Bitzz121 <abitsolihin8@gmail.com>
2022-10-21 18:56:43 +0700

    adding loading when fetch api

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,13 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { useState, useEffect } from 'react';
 import Link from 'next/link'
 import Head from 'next/head'
+import axios from 'axios'
 
 import '@splidejs/react-splide/css';
 
 export default function Index() {
   const [films, setFilm] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const getFilms = async () => {
@@ -14,6 +16,8 @@ export default function Index() {
       )
       const data = await response.json()
       setFilm(data)
+      setLoading(false)
+      console.log(data)
     }
     getFilms()
   }, [])
@@ -46,9 +50,91 @@ export default function Index() {
       <div className="container max-w-[100vw] min-h-[100%]">
         <div className="section h-[70vh] w-full">
           <div className="background-container w-full h-full relative">
-            <video autoPlay muted loop className="object-cover absolute w-full h-full">
+            <div className="video-wrapper  absolute w-full h-full">
+            <video autoPlay muted loop className="object-cover w-full h-full">
               <source src="trailer.mp4" type="video/mp4" />
-            </video>
+              </video>
+            </div>
+            {loading ? (
+              <div className='h-full w-full flex items-center justify-center'><h1 className='text-4xl text-white z-40'>Loading...</h1></div> ) : (
+                <Splide options={{
+                  breakpoints: {
+                    2560: {
+                      perPage: 3,
+                      padding: '5rem',
+                      gap: '30px',
+                      type: 'loop',
+                      pagination: true,
+                      arrows: false,
+                    },
+                    1920: {
+                      perPage: 2,
+                      padding: '5rem',
+                      gap: '30px',
+                      type: 'loop',
+                      pagination: true,
+                      arrows: false,
+                    },
+                    1028: {
+                      perPage: 1,
+                      padding: '15rem',
+                      gap: '15px',
+                      type: 'loop',
+                      pagination: true,
+                      arrows: false,
+                    },
+                    820: {
+                      perPage: 1,
+                      padding: '8rem',
+                      type: 'loop',
+                      gap: '10px',
+                      pagination: false,
+                      arrows: false,
+                    },
+                    480: {
+                      perPage: 1,
+                      padding: '3rem',
+                      type: 'loop',
+                      gap: '10px',
+                      pagination: false,
+                      arrows: false,
+                    },
+                  },
+                }} className="card-container absolute h-full w-full top-0 z-30 flex items-center p-0 justify-center text-white">
+                  {films.map((item) => {
+                    const { id, name, sinopsis, rating, url } = item
+                    return (
+                      <SplideSlide className="card duration-200 w-full h-[70%] md:h-[300px] 2xl:h-[400px] bg-gray-600/25 rounded-xl backdrop-blur-xl flex items-center justify-center" key={id}>
+                        <div className="article w-[60%] h-full p-2">
+                          <div className="title md:h-[30%] h-[50px]  w-full flex items-center text-xl md:text-3xl ">
+                            <h1 className='p-2 truncate'>{name}</h1>
+                          </div>
+                          <div className="description h-[90px] md:h-[120px] 2xl:h-[50%] overflow-ellipsis w-full text-xs 2xl:text-base md:text-sm flex items-center md:items-start font-light p-2">
+                            <p className="h-[80px] md:h-[120px] 2xl:h-full overflow-ellipsis overflow-y-hidden">{sinopsis}</p>
+                          </div>
+                          <div className="rating-button h-[25%] 2xl:h-[20%] w-full flex items-center p-2">
+                            <div className="rating w-[50%] hidden md:flex items-center">
+                              <div className="flex items-center justify-center text-white">
+                                <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                                <p className="ml-1 text-sm text-white">{rating} / 5</p>
+                              </div>
+                            </div>
+                            <div className="button w-full md:w-[50%] flex md:justify-end items-center">
+                              <Link href="/"><a className="px-5 py-2 bg-red-600 text-xs rounded">Nonton</a></Link>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="img-wrapper w-[40%] h-[170px] md:h-[100%] flex justify-center p-4">
+                          <div style={{ backgroundImage: `url(${url})` }} className="rounded-xl w-full h-full bg-cover bg-center"
+                          ></div>
+                        </div>
+                      </SplideSlide>
+                    )
+                  })}
+                </Splide>
+              ) 
+            }
+          
           </div>
           <div className="section h-[40vh] text-white ">
             <div className="title-wrapper flex p-4">
@@ -93,8 +179,7 @@ export default function Index() {
                 {films.map((film) => {
                   const { id, name, genre, url, rating } = film
                   return (
-                    <Link href="/" key={id}>
-                      <SplideSlide className="card h-full">
+                      <SplideSlide className="card h-full" key={id}>
                         <div style={{ backgroundImage: `url(${url})` }} className="img-wrapper overflow-hidden relative h-[150px] w-[100px] md:h-[180px] md:w-[130px] 2xl:h-[200px] 2xl:w-[150px] bg-cover bg-center rounded-md group after:content-[''] after:absolute after:z-10 after:hover:bottom-0 after:-bottom-80 after:duration-200 after:left-0 after:w-full after:h-full after:bg-gradient-to-t after:from-[#121212] after:to-[#121212]/25 after:rounded-md cursor-pointer"
                         >
                           <div className="kategori absolute right-0 top-0 text-xs px-2 py-1 bg-red-600 rounded-tr-md rounded-bl-md z-30">
@@ -109,7 +194,6 @@ export default function Index() {
                           </div>
                         </div>
                       </SplideSlide>
-                    </Link>
                   )
                 })}
               </Splide>
